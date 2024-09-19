6b8ec0bd7f04fc635f672ebc6ed45cee07c493d6
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-09-19 21:25:22 +0300

    Part05 DepartmentModule and DbContext

--- Route.IKEA.DAL/Presistence/Data/ApplicationDbContext.cs
+++ Route.IKEA.DAL/Presistence/Data/ApplicationDbContext.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Route.IKEA.DAL.Entities.Department;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,13 @@ namespace Route.IKEA.DAL.Presistence.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-             
+            optionsBuilder.UseSqlServer("Server=.; DataBase=CompanyIKEA; Trusted_Connection=True; TrustServerCertificate=True;");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
     }
 }
---
+++ Route.IKEA.DAL/Presistence/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Route.IKEA.DAL.Entities.Department;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.IKEA.DAL.Presistence.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+            builder.Property(D => D.Name).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(D => D.Code).HasColumnType("varchar(20)").IsRequired();
+
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("GETDATEUTC()");
+            builder.Property(D => D.CreatedOn).HasDefaultValueSql("GETDATEUTC");
+
+
+
+
+        }
+    }
+}
