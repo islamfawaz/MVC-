0a3a778a87ff0a10e0d0041ae75fadcb6fcfa801
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-09-19 20:41:36 +0300

    Add project files.

---
+++ Route.IKEA.DAL/Presistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.IKEA.DAL.Presistence.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Route.IKEA.PL/Program.cs
@@ -0,0 +1,41 @@
+namespace Route.IKEA.PL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Services
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            #endregion
+
+            var app = builder.Build();
+
+            #region Configure Kestrel Middlewares
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            #endregion
+            app.Run();
+        }
+    }
+}
